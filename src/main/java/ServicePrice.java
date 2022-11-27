876601bd4f62bd0d51e74118f1869a9ee2837444
Andrey0012 <andrey.0012@mail.ru>
2022-11-27 21:19:26 +0300

    commit sav

--- src/main/java/ServicePrice.java
+++ src/main/java/ServicePrice.java
@@ -1,63 +1,60 @@
-import javafx.util.Pair;
+import org.apache.commons.lang3.tuple.Pair;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ServicePrice {
 
-    public static List<Price> resultMethod(List<Price> mainCollectionAv, List<Price> mainCollectionNew) {
-        // итоговые цены
-        List<Price> priceList = new ArrayList<>();
+    public List<Price> resultMethod(List<Price> mainCollectionAv, List<Price> mainCollectionNew) {
+        List<Price> priceListResult = new ArrayList<>();
 
-        List<Pair> collect2 = mainCollectionNew.stream().map(price -> {
+        List<Pair<Price, Price>> collect2 = mainCollectionNew.stream().map(price -> {
             int i = mainCollectionAv.indexOf(price);
-            if (!(i >= 0)) {
-                //добавляем новую цену без сранения если такой цены не было
-                priceList.add(price);
-            }
             if (i >= 0) {
-                return new Pair(price, mainCollectionAv.get(i));
+                return Pair.of(price, mainCollectionAv.get(i));
             }
+            priceListResult.add(price);
             return null;
         }).filter(Objects::nonNull).collect(Collectors.toList());
 
-        //добавляем все со старой цены если ничего нового по позиции не было
-        mainCollectionAv.stream().peek(price -> {
+        mainCollectionAv.forEach(price -> {
             int i = mainCollectionNew.indexOf(price);
-            if (!(i >= 0)) {
-                priceList.add(price);
+            if (i < 0) {
+                priceListResult.add(price);
             }
-        }).collect(Collectors.toList());
+        });
 
-        for (int i = 0; i < collect2.size(); i++) {
-            Pair pair = collect2.get(i);
-            Price key = (Price) pair.getKey();
-            Price value = (Price) pair.getValue();
-            if (key.getValue() == value.getValue()) {
-                List<LocalDateTime> begin = List.of(key.getBegin(), value.getBegin());
-                LocalDateTime localDateTime1Begin = begin.stream().min((x, y) -> x.compareTo(y)).get();
-                List<LocalDateTime> end = List.of(key.getEnd(), value.getEnd());
-                LocalDateTime localDateTimeEnd = end.stream().max((x, y) -> x.compareTo(y)).get();
-                Price price = new Price(key.getId(), key.getProductCode(), key.getNumber(),
-                        key.getDepart(), localDateTime1Begin, localDateTimeEnd, key.getValue());
-                priceList.add(price);
+        for (Pair<Price, Price> pair : collect2) {
+            Price newValue = pair.getKey();
+            Price availableValue = pair.getValue();
+            if (newValue.getValue() == availableValue.getValue()) {
+                Price price = valueEqual(newValue, availableValue);
+                priceListResult.add(price);
             } else {
-                priceList.add(key);
-                if (key.getBegin().isAfter(value.getBegin())) {
-                    Price price = new Price(value.getId(), value.getProductCode(), value.getNumber(),
-                            value.getDepart(), value.getBegin(), key.getBegin(), value.getValue());
-                    priceList.add(price);
+                priceListResult.add(newValue);
+                if (newValue.getBegin().isAfter(availableValue.getBegin())) {
+                    Price price = new Price(availableValue.getId(), availableValue.getProductCode(), availableValue.getNumber(),
+                            availableValue.getDepart(), availableValue.getBegin(), newValue.getBegin(), availableValue.getValue());
+                    priceListResult.add(price);
                 }
-                if (value.getEnd().isAfter(key.getEnd())) {
-                    Price price = new Price(value.getId(), value.getProductCode(), value.getNumber(),
-                            value.getDepart(), key.getEnd(), value.getEnd(), value.getValue());
-                    priceList.add(price);
+                if (availableValue.getEnd().isAfter(newValue.getEnd())) {
+                    Price price = new Price(availableValue.getId(), availableValue.getProductCode(), availableValue.getNumber(),
+                            availableValue.getDepart(), newValue.getEnd(), availableValue.getEnd(), availableValue.getValue());
+                    priceListResult.add(price);
                 }
             }
         }
-        return priceList;
+        return priceListResult;
+    }
+
+    public static Price valueEqual(Price key, Price value) {
+        LocalDateTime begin = Stream.of(key.getBegin(), value.getBegin()).min(LocalDateTime::compareTo).get();
+        LocalDateTime end = Stream.of(key.getEnd(), value.getEnd()).max(LocalDateTime::compareTo).get();
+        return new Price(key.getId(), key.getProductCode(), key.getNumber(),
+                key.getDepart(), begin, end, key.getValue());
     }
 }
